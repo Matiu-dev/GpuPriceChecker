f133467642fce214ec83bf82034a682f11662572
Matiu-dev <matiw97@wp.pl>
2022-07-22 17:44:14 +0200

    creating backend

---
+++ GpuPriceCheckerBackEnd/src/main/java/pl/matiu/gpupricechecker/GpupricecheckerApplication.java
@@ -0,0 +1,13 @@
+package pl.matiu.gpupricechecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpupricecheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpupricecheckerApplication.class, args);
+	}
+
+}
---
+++ GpuPriceCheckerBackEnd/src/test/java/pl/matiu/gpupricechecker/GpupricecheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.matiu.gpupricechecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpupricecheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
